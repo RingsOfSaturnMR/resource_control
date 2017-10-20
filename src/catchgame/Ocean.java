b81d9e839e539f0c4315bda477fdaee2a7d19d9c
tlgit11 <ltdxxii@gmail.com>
2017-10-20 14:29:44 -0400

    Add files via upload
    Updating

--- src/catchgame/Ocean.java
+++ src/catchgame/Ocean.java
@@ -3,6 +3,8 @@ package catchgame;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import resources.Fish;
 import resources.FishSpecies;
@@ -25,42 +27,65 @@ public class Ocean implements Serializable
 	
 	// for randomly generating weights
 	private Random rand = new Random();
+	private double relativeGrowthRate=0.02;
 	
+
+
+
+
 	public Ocean()
 	{
-		// populate each array list with appropriate SeaCreatures.
-		// Randomly generate each SeaCreature's weight by randomly generating a number between
-		// the INITIAL_WEIGHT_MIN and INITAL_WEIGHT_MAX
-		// get values from catchgame.Constant
-		//
-		//maybe use a timeline to regenrate fish if isfh is less than max population
-
-		// this includes max_population for each fish
-		// if they are not there, please add them
-		// also, ocean should somehow regnerate it's population over time
-		// this would require a formula
-		// and some time related feature
-		// maybe try the java class Timeline?
-		// for the formula could use the formula in matt_branch in seafoodPopulation.java
-		// different from seaCreature.java
-		// formula is called getPopulationWithGrowth
-		// would have to get the number of seacreatures to add to each array list,
-		// using a formula like getPopulationWithGrowth
-		// and then add them to each array list
 		System.out.println("In the Ocean Constructor");
+		boolean newGame=true;
+		if (newGame) {
+			fillOceanInitially();
+		}
+		else {
+			//load old ocean
+		}
+		regenerateOcean();
+			
+		
 	}
-	/*
-	add to population function
+	
+	public void regenerateOcean() {
+		Timer timer = new Timer();
+		TimerTask task;
+		task = new TimerTask() {
+
+	        @Override
+	        public void run() { 
+	        int codToAdd=updateCodPopulation(10);
+	        addABunchOfFish(codPopulation, FishSpecies.COD, codToAdd);
+	        }
+	        	
+	    };
+	    timer.schedule(task, 0, 5_000);
+	}
+	
+	private int updateCodPopulation(long elapsedTime) {
+        int carryingCapacityPopulation=Constants.COD_MAX_POPULATION;
+        int lastPopulation=codPopulation.size();
+        double A=(double)(carryingCapacityPopulation-lastPopulation)/(double)lastPopulation;
+        double denominator=1+A*Math.exp(-relativeGrowthRate*elapsedTime);
+        double rawPopulation=(double)carryingCapacityPopulation/denominator;
+        //make it an int becuase we can't have (viable) fractions of seafood
+        int updatedPopulation=(int)rawPopulation;
+        //next line for debug
+        System.out.println(updatedPopulation);
+        return updatedPopulation-codPopulation.size();
+    }
+	/*add to population function
 	populationAmount, arraytlist, while arraylist is less than population, adds a fish
 		call add a fish function in add to population function
 	
-	add fish
-	enum fish type, max weight, min weight
-	returns a fish of that kind between min and max weight using random number between min and max
+	//add fish
+	//enum fish type, max weight, min weight
+	//returns a fish of that kind between min and max weight using random number between min and max
 	
-	random function for returning weight between max and min
+	//random function for returning weight between max and min
 	
-	add to population function using initial population size for all species in ocean
+	//add to population function using initial population size for all species in ocean
 	
 	return a packet of fish, takes as parameter a plentifulnes variable, also subtracts fish from ocean
 	
@@ -78,21 +103,27 @@ public class Ocean implements Serializable
 	it should take for time just some constant that you choose
 	}
 	
-	@Override
-    protected final int getPopulationRegneration(long elapsedTime, rawPopulation, mxaPopulation) {
-        int carryingCapacityPopulation=maxPopulation
-        int lastPopulation=rawPopulation
+	//this is a general algorithim for a formula which will tell you the new population
+    @Override
+    protected final int getPopulationWithGrowth(long elapsedTime) {
+        int carryingCapacityPopulation=getNormalPopulation();
+        int lastPopulation=getPopulation();
         double A=(double)(carryingCapacityPopulation-lastPopulation)/(double)lastPopulation;
         double denominator=1+A*Math.exp(-relativeGrowthRate*elapsedTime);
         double rawPopulation=(double)carryingCapacityPopulation/denominator;
-        return rawPopulation;
+        //make it an int becuase we can't have (viable) fractions of seafood
+        int updatedPopulation=(int)rawPopulation;
+        //next line for debug
+        //System.out.println(updatedPopulation);
+        return updatedPopulation;
     }
-    
-    updateSeaCreaturePopulation(arraylist seacreature, species){
-    add until (int)rawpopulation==arraylist.length
+
 	*/
 	
 	
+
+
+
 	public SeaCreature extractRandomSeaCreature()
 	{
 		// STUBB
@@ -115,6 +146,49 @@ public class Ocean implements Serializable
 		default:
 			return null;
 		}
+	}
+	
+	public ArrayList<Fish> givePacketOfFish(FishSpecies fish, int currentPopulation, int maxPopulation ) throws Exception{
+		double clientPlentifullness=(double)currentPopulation/(double)maxPopulation;
+		ArrayList<Fish> fishPacket=new ArrayList<>();
+		switch (fish){
+		case COD:
+			double oceanPlentifullness=codPopulation.size()/Constants.COD_MAX_POPULATION;
+			return getPacketOfFish(codPopulation, 
+					oceanPlentifullness, clientPlentifullness, maxPopulation, currentPopulation);
+			//break;
+		}
+		
+		return fishPacket;
+	}
+	
+	public ArrayList<Fish> getPacketOfFish(ArrayList<Fish> fishPopulation, 
+			double oceanPlentifullness, double clientPlentifullness, int maxPopulation, int currentPopulation)throws Exception{
+		if (oceanPlentifullness>clientPlentifullness) {
+			int numFish=(int)(oceanPlentifullness/maxPopulation)-currentPopulation;
+			return extractABunchOfFish(fishPopulation, numFish);
+		}
+		else {
+			ArrayList<Fish> fishPacket=new ArrayList<>();
+			return fishPacket;
+		}
+	}
+	
+	
+	public ArrayList<Fish>extractABunchOfFish(ArrayList<Fish>fishPopulation, int num) throws Exception{
+		ArrayList<Fish>extractedFishes=new ArrayList<Fish>();
+		for (int i=0; i<=num-1; i++) {
+			extractedFishes.add(extractFish(fishPopulation));
+		}
+		
+		return extractedFishes;
+	}
+	
+	public Fish extractFish(ArrayList<Fish>fishPopulation) throws Exception{
+		Fish fish=fishPopulation.get(fishPopulation.size()-1);
+		fishPopulation.remove(fishPopulation.size()-1);
+		
+		return fish;
 	}
 	
 	public Fish extractCod()
@@ -145,6 +219,21 @@ public class Ocean implements Serializable
 	public Shellfish extractOyster()
 	{
 		return new Shellfish(ShellfishSpecies.OYSTER, 1);
+	}
+	
+	public void fillOceanInitially() {
+		addABunchOfFish(codPopulation, FishSpecies.COD, Constants.COD_INITIAL_POPULATION);
+		//add for all other populations
+	}
+	
+	public void addABunchOfFish(ArrayList<Fish>fishPopulation, FishSpecies species, int num) {
+		for (int i=0; i<=num-1; i++) {
+			addFish(fishPopulation, species);
+		}
+	}
+	
+	public void addFish(ArrayList<Fish>fishPopulation, FishSpecies species) {
+		fishPopulation.add(new Fish(species, getFishSpeciesRandomWeight(species)));
 	}
 	
 	public int getCurrentCodPopulation()
@@ -184,10 +273,53 @@ public class Ocean implements Serializable
 	}
 	
 	
-	// helper function
+	// get random weight of each species
+	public double getCodWeight() {
+		double random = getRandomDouble(Constants.COD_INITIAL_WEIGHT_MIN, Constants.COD_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+		
+	public double getSalmonWeight() {
+			
+		double random = getRandomDouble(Constants.SALMON_INITIAL_WEIGHT_MIN, Constants.SALMON_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+	
+	public double getTunaWeight() {
+		double random = getRandomDouble(Constants.TUNA_INITIAL_WEIGHT_MIN, Constants.TUNA_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+		
+	public double getCrabWeight() {
+		double random = getRandomDouble(Constants.CRAB_INITIAL_WEIGHT_MIN, Constants.CRAB_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+		
+	public double getLobsterWeight() {
+		double random = getRandomDouble(Constants.LOBSTER_INITIAL_WEIGHT_MIN, Constants.LOBSTER_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+		
+	public double getOysterWeight() {
+		double random = getRandomDouble(Constants.OYSTER_INITIAL_WEIGHT_MIN, Constants.OYSTER_INITIAL_WEIGHT_MAX);
+		return random;
+	}
+	
+	public double getFishSpeciesRandomWeight(FishSpecies species) {
+		double weight=0;
+		switch (species) {
+		case COD: weight = getCodWeight(); break;
+		}
+		return weight;
+	}
+		// helper function
 	private int getRandomInt(int min, int max)
 	{
 		int randomInt = rand.nextInt(max) + min;
 		return randomInt;
+	}
+	private double getRandomDouble(double max, double min) {
+		double randomDouble = (max - min) * rand.nextDouble() + min;
+		return randomDouble;
 	}
 }
