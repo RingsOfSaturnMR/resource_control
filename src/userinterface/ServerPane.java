8b0ccf074f1ed45b8ddb9388c886e7924b9121f9
tlgit11 <ltdxxii@gmail.com>
2017-11-16 19:57:09 -0500

    for frequency histogram

--- src/userinterface/ServerPane.java
+++ src/userinterface/ServerPane.java
@@ -1,15 +1,24 @@
 package userinterface;
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 public class ServerPane extends Pane
 {
 	private TextArea taOutput = new TextArea("");
 	
-	public ServerPane()
+	private Button btnFH=new Button("Frequency Histogram Of Ocean");
+	
+	private VBox vB=new VBox();
+	
+	public ServerPane(EventHandler launch_FH_Action)
 	{
-		getChildren().addAll(taOutput);
+		btnFH.setOnAction(launch_FH_Action);
+		vB.getChildren().addAll(taOutput, btnFH);
+		getChildren().addAll(vB);
 	}
 	
 	public void appendToOutput(String str)
