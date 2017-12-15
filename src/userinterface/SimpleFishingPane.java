5828384d7140bf3f536725a113d078b70c7c8afb
RingsOfSaturnMR <31932179+RingsOfSaturnMR@users.noreply.github.com>
2017-12-15 00:37:07 -0500

    just ocean background

--- src/userinterface/SimpleFishingPane.java
+++ src/userinterface/SimpleFishingPane.java
@@ -3,36 +3,35 @@ package userinterface;
 
 
 import catchgame.Constants;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * A SimpleFishingPane of the width and height given in constants,
+ * and with an ocean image for a proportion of the screen, with
+ * the rest of the screen beneath it given a gray rectangle
+ * to look like a rocky sea floor
+ * @author mattroberts
+ *
+ */
 public class SimpleFishingPane extends Pane
 {
-	// temp for now to show professor miller where we are going with this.
-	// Pane explainPane = new Pane();
-	// Pane fishPane = new Pane();
+	
+	
 
-	// private int numCreaturesOnScreen = 0;
-	// private Button btnExtractFishAction = new Button("Extract Fish");
-	// private ArrayList<SeaCreature> creaturesOnScreen = new ArrayList<>();
-	private Label labelExplanation = new Label(
-			"Right now the fish are shown as dots. These dots are from SeaCreature objects, extracted " + "from an Ocean object belonging the server and sent to this client. We plan to animate them, and make it so that when they are " +
-					"clicked (or however else caught), they are added (By calling the function attatched to the button) to the players resouces array, " +
-					"which they can sell, to get better fishing equipment");
-
+	/**
+	 * Constructs a SimpleFishingPane of the width and height given 
+	 * in constants, and with an ocean image for a proportion of the 
+	 * screen, with the rest of the screen beneath it given a gray 
+	 * rectangle to look like a rocky sea floor
+	 */
 	public SimpleFishingPane()
 	{
 		this.setMinWidth(Constants.INITIAL_SIMPLE_FISHING_PANE_WIDTH);
 		this.setMinHeight(Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT);
-		labelExplanation.setMaxWidth(Constants.INITIAL_SIMPLE_FISHING_PANE_WIDTH);
-		labelExplanation.setWrapText(true);
-		// labelExplanation.setTranslateY(50);
-		// btnExtractFishAction.setOnAction(extractFishAction);
 		
 		Rectangle clippingRectangle=new Rectangle(Constants.INITIAL_SIMPLE_FISHING_PANE_WIDTH,
 				Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT);
@@ -40,12 +39,12 @@ public class SimpleFishingPane extends Pane
 		Image oceanImage=new Image("img/ocean.png");
 		oceanImageView.setImage(oceanImage);
 		oceanImageView.setFitWidth(Constants.INITIAL_SIMPLE_FISHING_PANE_WIDTH);
-		oceanImageView.setFitHeight(Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT*Constants.TOP_COEFFICIENT);
+		oceanImageView.setFitHeight(Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT/*Constants.TOP_COEFFICIENT*/);
 		Rectangle rocksRectangle=new Rectangle(Constants.INITIAL_SIMPLE_FISHING_PANE_WIDTH,
 				Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT*Constants.BOTTOM_COEFFICIENT);
 		rocksRectangle.setTranslateY(Constants.INITIAL_SIMPLE_FISHING_PANE_HEIGHT*Constants.TOP_COEFFICIENT);
 		rocksRectangle.setFill(Color.LIGHTGREY);
-		this.getChildren().addAll(oceanImageView, rocksRectangle);
+		this.getChildren().addAll(oceanImageView/*, rocksRectangle*/);
 		this.setClip(clippingRectangle);
 	}
 }
