71d13f51a13eddc7f914fdb27a29b0511c40344e
Nils Johnson <nilsjohnson328@gmail.com>
2017-12-15 00:59:30 -0500

    added to readme, added unit test

--- src/catchgametest/AuthenticatorTest.java
+++ src/catchgametest/AuthenticatorTest.java
@@ -1,6 +1,7 @@
 package catchgametest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +13,15 @@ public class AuthenticatorTest
 	@Test
 	public void testEntryptionFilterShouldEncryptsAndDecyrpts()
 	{
-		assertEquals("Hello", EncryptionFilter.encrypt("Hello", "Hello"), "Hello Should entrypt to hello");
+		String plainText = "Moose are the largest of all the deer species. Males are immediately recognizable by their huge antlers, which can spread 6 feet from end to end.";
+		String key = EncryptionFilter.getRandomKey();
+		
+		String cipherText = EncryptionFilter.encrypt(plainText, key);
+		String deCipheredText = EncryptionFilter.decrypt(cipherText, key);
+				
+		System.out.println(plainText);
+		System.out.println(deCipheredText);
+				
+		assertTrue("A String should be able to encrypt and dycrpt", plainText.equals(deCipheredText));
 	}
-	
 }
